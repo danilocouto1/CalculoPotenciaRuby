ebc0124a567cd96536df879bf8dffb18ce48191a
Yehior <dansantosalfa@gmail.com>
2023-11-11 15:32:26 -0300

    feat: Valor da potencia e for com calculo e saida de dados

--- Potencia.rb
+++ Potencia.rb
@@ -6,3 +6,11 @@ while contador <= 3
     numeros.push gets.chomp.to_i
   contador += 1
 end
+
+potencia = 0
+    puts "Digite a potencia a ser usada: "
+    potencia = gets.chomp.to_i
+for numero in numeros
+  numero = numero**potencia
+  puts "Numero elevado a #{potencia}º potencia é #{numero}"
+end
